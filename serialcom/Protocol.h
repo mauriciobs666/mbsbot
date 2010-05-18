480ab8ae264ebd6d8df4e211d3fe842ba8ffb7b3
mauriciobs <mauriciobs@3c29fe3e-da84-11de-ae71-c59d11f48c9c>
2010-05-18 01:45:48 +0000

    Missing file
    git-svn-id: https://mbs-bot.googlecode.com/svn/trunk@71 3c29fe3e-da84-11de-ae71-c59d11f48c9c

---
+++ serialcom/Protocol.h
@@ -0,0 +1,31 @@
+/*
+ *	Copyright (C) 2010 - Mauricio Bieze Stefani
+ *	This file is part of the MBSBOT project.
+ *
+ *	MBSBOT is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	MBSBOT is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with MBSBOT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PROTOCOL_H_INCLUDED
+#define PROTOCOL_H_INCLUDED
+
+enum ProgramID
+{
+	PRG_RC = 0,
+	PRG_SHOW_SENSORS,
+	PRG_PHOTOVORE,
+	PRG_LINEFOLLOWER,
+	PRG_SHARP
+};
+
+#endif // PROTOCOL_H_INCLUDED
