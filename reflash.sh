caacfa5fefed1b15568b4162d78b761227ae5a69
mauriciobs <mauriciobs@3c29fe3e-da84-11de-ae71-c59d11f48c9c>
2010-12-28 11:37:22 +0000

    Error handling
    git-svn-id: https://mbs-bot.googlecode.com/svn/trunk@135 3c29fe3e-da84-11de-ae71-c59d11f48c9c

--- reflash.sh
+++ reflash.sh
@@ -1,5 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# http://www.ladyada.net/learn/avr/avrdude.html
+# (C) GPL 2010 - Mauricio Bieze Stefani
+# Based on info from http://www.ladyada.net/learn/avr/avrdude.html
 
-$ARDUINO_DIR/hardware/tools/avrdude -C$ARDUINO_DIR/hardware/tools/avrdude.conf -v -v -patmega328p -cstk500v1 -P$1 -b57600 -D -Uflash:w:$2:i 
+if [ ! -x $ARDUINO_DIR/hardware/tools/avrdude ]; then 
+    if [ -z $ARDUINO_DIR ]; then
+        echo "export ARDUINO_DIR="
+        exit
+    else
+        echo "$ARDUINO_DIR/hardware/tools/avrdude is not an executable"
+	exit
+    fi
+fi
+
+if [ $# -lt 2 ]; then 
+    echo "Usage: reflash.sh <PORT> <FILE.HEX>"
+    exit
+fi
+
+$ARDUINO_DIR/hardware/tools/avrdude -C$ARDUINO_DIR/hardware/tools/avrdude.conf -v -v -patmega328p -cstk500v1 -P$1 -b57600 -D -Uflash:w:$2:i
+
