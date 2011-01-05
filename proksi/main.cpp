c0b75a77c9a3288ea76eddd945894fa13a1192c8
mauriciobs <mauriciobs@3c29fe3e-da84-11de-ae71-c59d11f48c9c>
2011-01-05 03:21:24 +0000

    Serial port using boost tests
    git-svn-id: https://mbs-bot.googlecode.com/svn/trunk@136 3c29fe3e-da84-11de-ae71-c59d11f48c9c

--- proksi/main.cpp
+++ proksi/main.cpp
@@ -1,21 +1,71 @@
 #include <MBSUtil.h>
 #include <iostream>
+#include <boost/asio.hpp>
 
-using namespace std;
+#ifdef _WIN32
+    #define DEFAULT_SERIAL_DEVICE "COM1"
+#else
+	#define DEFAULT_SERIAL_DEVICE "/dev/ttyUSB0"
+#endif
+
+std::string serialPortDevice(DEFAULT_SERIAL_DEVICE);
+int baudRate = 115200;
+unsigned short listenPort = 6661;
 
 int main(int argc, char* argv[])
 {
-    int argSeq = 0;
-
     for(int a=1; a<argc; a++)
     {
-        switch(argv[a][0])
+        if( argv[a][0] == '-' )
         {
-            case '-':
-            break;
-            default:
-            break;
+            switch(argv[a][1])
+            {
+                case '?':
+                    std::cout << "Usage:" << std::endl << "proksi <device> <tcp port>" << std::endl;
+                break;
+            }
         }
+        else if( isalpha(argv[a][0]) || (argv[a][0] == '/') )
+        {
+            serialPortDevice = argv[a];
+        }
+        else if( isdigit(argv[a][0]) )
+        {
+            listenPort = atoi(argv[a]);
+        }
+        else
+        {
+            TRACE_ERROR("WTF %s", argv[a]);
+        }
+    }
+
+    boost::asio::io_service io;
+
+    try
+    {
+        boost::asio::serial_port serial(io, serialPortDevice);
+        serial.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+        TRACE_INFO("Opened %s @ %d bps", serialPortDevice.c_str(), baudRate);
+        char c;
+        std::string result;
+        for(;;)
+        {
+            boost::asio::read(serial, boost::asio::buffer(&c,1));
+            switch(c)
+            {
+                case '\r':
+                    break;
+                case '\n':
+                    std::cout << result;
+                    result.clear();
+                default:
+                    result+=c;
+            }
+        }
+    }
+    catch(boost::system::system_error &e)
+    {
+        TRACE_ERROR("Error while trying to open %s: \"%s\"", serialPortDevice.c_str(), e.what());
     }
 
     return 0;
