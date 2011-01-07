92290a908843bd5f79375e6bf61c13955134c605
mauriciobs <mauriciobs@3c29fe3e-da84-11de-ae71-c59d11f48c9c>
2011-01-07 02:36:21 +0000

    Proksi almost fully functional (looses some replies)
    git-svn-id: https://mbs-bot.googlecode.com/svn/trunk@139 3c29fe3e-da84-11de-ae71-c59d11f48c9c

--- proksi/main.cpp
+++ proksi/main.cpp
@@ -24,42 +24,54 @@
 #include <boost/program_options.hpp>
 #include <boost/thread.hpp>
 
+using namespace std;
+using namespace boost;
+
 #ifdef _WIN32
     #define DEFAULT_SERIAL_DEVICE "COM1"
 #else
 	#define DEFAULT_SERIAL_DEVICE "/dev/ttyUSB0"
 #endif
+std::string serialPortDevice(DEFAULT_SERIAL_DEVICE);
 
 #define DEFAULT_SERIAL_BAUD 115200
-#define DEFAULT_LISTEN_PORT 6661
-
-std::string serialPortDevice(DEFAULT_SERIAL_DEVICE);
 int baudRate = DEFAULT_SERIAL_BAUD;
+
+#define DEFAULT_LISTEN_PORT 6661
 int listenPort = DEFAULT_LISTEN_PORT;
 
-using namespace std;
-using namespace boost;
-namespace po = boost::program_options;
+#define SERIAL_BUFFER_SIZE 32
+char serialBufferRx[SERIAL_BUFFER_SIZE];
+char serialBufferTx[SERIAL_BUFFER_SIZE];
 
-void print_rx(asio::serial_port &p)
+asio::io_service io;
+asio::serial_port serial(io);
+asio::ip::tcp::socket tcpsocket(io);
+
+void handleRx(const system::error_code& error, size_t n)
 {
-    char c;
-    for(;;)
-    {
-        asio::read(p, asio::buffer(&c,1));
-        cout << c;
-    }
+    cout << std::string(serialBufferRx,n);
+    if(tcpsocket.is_open())
+        asio::write(tcpsocket, asio::buffer(serialBufferRx,n));
+    serial.async_read_some(asio::buffer(serialBufferRx,SERIAL_BUFFER_SIZE), &handleRx);
+}
+
+void handleTx(const system::error_code& error, size_t n)
+{
+    cout << std::string(serialBufferTx,n);
+    asio::write(serial, asio::buffer(serialBufferTx,n));
+    tcpsocket.async_read_some(asio::buffer(serialBufferTx,SERIAL_BUFFER_SIZE), &handleTx);
 }
 
 int main(int argc, char* argv[])
 {
     cout << "TCP/IP to Serial port proxy" << endl;
-    cout << "(c) GPL 2011 - Mauricio Bieze Stefani" << endl;
+    cout << "(c) GPL 2011 - Mauricio Bieze Stefani" << endl << endl;
 
-    // parse command line options
-
-    try
+    try // parse configuration
     {
+        namespace po = boost::program_options;
+
         po::options_description desc("Usage");
 
         desc.add_options()
@@ -69,22 +81,26 @@ int main(int argc, char* argv[])
             ("port,p", po::value<int>(&listenPort)->default_value(DEFAULT_LISTEN_PORT), "TCP port to listen")
             ;
 
-        po::positional_options_description p;
-        p.add("device", -1);
-
         po::variables_map vm;
 
-        ifstream ifs("proksi.cfg");
-        if (!ifs)
+        // use config file if available
+        ifstream cfg_file("proksi.cfg");
+        if (cfg_file.good())
         {
-            // error ? i dont think so...
-        }
-        else
-        {
-            store(parse_config_file(ifs, desc), vm);
-            notify(vm);
+            TRACE_INFO("Using config file proksi.cfg");
+            po::store(po::parse_config_file(cfg_file, desc), vm);
+            po::notify(vm);
         }
 
+        po::positional_options_description p;
+
+        // use first alone argument as "device"
+        p.add("device", 1);
+
+        // use second alone argument as "port"
+        p.add("port", 1);
+
+        // parse command line
         po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
         po::notify(vm);
 
@@ -96,24 +112,42 @@ int main(int argc, char* argv[])
     }
     catch(...)
     {
+        TRACE_ERROR("Unknown exception");
         exit(-1);
     }
 
-    try
+    try // open serial port
     {
-        asio::io_service io;
-        asio::serial_port serial(io);
         serial.open(serialPortDevice);
         serial.set_option(asio::serial_port_base::baud_rate(baudRate));
-
         TRACE_INFO("Opened %s @ %d bps", serialPortDevice.c_str(), baudRate);
-
-        boost::thread t(boost::bind(&print_rx, boost::ref(serial)));
-        t.join();
     }
     catch(boost::system::system_error &e)
     {
         TRACE_ERROR("Error while trying to open %s: \"%s\"", serialPortDevice.c_str(), e.what());
+        exit(-2);
+    }
+
+    try // listen and accept connections
+    {
+        asio::ip::tcp::acceptor acceptor(io, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), listenPort));
+
+        TRACE_INFO("Listening port %d for conections...", listenPort);
+
+        for (;;)
+        {
+            acceptor.accept(tcpsocket);
+
+            serial.async_read_some(asio::buffer(serialBufferRx,SERIAL_BUFFER_SIZE), &handleRx);
+            tcpsocket.async_read_some(asio::buffer(serialBufferTx,SERIAL_BUFFER_SIZE), &handleTx);
+            io.run();
+
+            tcpsocket.close();
+        }
+    }
+    catch(std::exception &e)
+    {
+        TRACE_ERROR("shit => %s", e.what());
     }
 
     return 0;
