3dd8cfcec5ea9d6ef0ed96249e689699a266ed2b
Mauricio B Stefani <mauriciobs@gmail.com>
2015-09-06 19:11:56 -0300

    matlab week 6 ex 9
    fim da week 6

--- coursera/matlab/square_wave.m
+++ coursera/matlab/square_wave.m
@@ -1,6 +1,13 @@
-function square_wave( n )
+function v = square_wave( n )
+    
+    v = zeros(1,1001);
+    
+    fator = ( 4 * pi ) / 1001;
+    
     for t = 1:1001
         for k = 1:n
-            
+            v(1,t) = v(1,t) + ( sin( (2*k-1) * t ) / ( 2*k -1 ) );
         endfor;
     endfor;
+    
+    plot(v);
